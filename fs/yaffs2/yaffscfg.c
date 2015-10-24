937a2957aefb71e698aca68078143a17f98ce11d
xianb <txianb@gmail.com>
2015-10-25 00:30:29 +0800

    Revert "compiled success"
    This reverts commit 2d7c066b58e4276853217485dc0309fb96effd01.

--- fs/yaffs2/yaffscfg.c
+++ fs/yaffs2/yaffscfg.c
@@ -1,31 +1,51 @@
 /*
- * YAFFS: Yet another FFS. A NAND-flash specific file system.
+ * YAFFS: Yet Another Flash File System. A NAND-flash specific file system.
+ *
+ * Copyright (C) 2002-2007 Aleph One Ltd.
+ *   for Toby Churchill Ltd and Brightstar Engineering
+ *
+ * Created by Charles Manning <charles@aleph1.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+/*
  * yaffscfg.c  The configuration for the "direct" use of yaffs.
  *
  * This file is intended to be modified to your requirements.
  * There is no need to redistribute this file.
  */
 
+/* XXX U-BOOT XXX */
+#include <common.h>
+
 #include <config.h>
+#include "nand.h"
 #include "yaffscfg.h"
 #include "yaffsfs.h"
-#include "nand.h"
+#include "yaffs_packedtags2.h"
 #include "yaffs_mtdif.h"
-//#include <errno.h>
+#include "yaffs_mtdif2.h"
+#if 0
+#include <errno.h>
+#else
+#include "malloc.h"
+#endif
 
-unsigned yaffs_traceMask = 0xFFFFFFFF;
-
-
-static int yaffs_errno;
-
-static int isMounted = 0;
-#define MOUNT_POINT "/flash"
-extern nand_info_t nand_info[];
+unsigned yaffs_traceMask = 0x0; /* Disable logging */
+static int yaffs_errno = 0;
 
 void yaffsfs_SetError(int err)
 {
 	//Do whatever to set error
 	yaffs_errno = err;
+}
+
+int yaffsfs_GetError(void)
+{
+	return yaffs_errno;
 }
 
 void yaffsfs_Lock(void)
@@ -41,6 +61,16 @@ __u32 yaffsfs_CurrentTime(void)
 	return 0;
 }
 
+void *yaffs_malloc(size_t size)
+{
+	return malloc(size);
+}
+
+void yaffs_free(void *ptr)
+{
+	free(ptr);
+}
+
 void yaffsfs_LocalInitialisation(void)
 {
 	// Define locking semaphore.
@@ -53,53 +83,75 @@ void yaffsfs_LocalInitialisation(void)
 // NB Though /boot and /flash occupy the same physical device they
 // are still disticnt "yaffs_Devices. You may think of these as "partitions"
 // using non-overlapping areas in the same device.
-// 
+//
 
-//#include "yaffs_ramdisk.h"
-//#include "yaffs_flashif.h"
+#include "yaffs_ramdisk.h"
+#include "yaffs_flashif.h"
 
+static int isMounted = 0;
+#define MOUNT_POINT "/flash"
+extern nand_info_t nand_info[];
+
+/* XXX U-BOOT XXX */
+#if 0
 static yaffs_Device ramDev;
 static yaffs_Device bootDev;
 static yaffs_Device flashDev;
+#endif
 
 static yaffsfs_DeviceConfiguration yaffsfs_config[] = {
-
-//	{ "/ram", &ramDev},
-//	{ "/boot", &bootDev},
+/* XXX U-BOOT XXX */
+#if 0
+	{ "/ram", &ramDev},
+	{ "/boot", &bootDev},
 	{ "/flash", &flashDev},
+#else
+	{ MOUNT_POINT, 0},
+#endif
 	{(void *)0,(void *)0}
 };
 
 
 int yaffs_StartUp(void)
 {
+	struct mtd_info *mtd = &nand_info[0];
+	int yaffsVersion = 2;
+	int nBlocks;
+
+	yaffs_Device *flashDev = calloc(1, sizeof(yaffs_Device));
+	yaffsfs_config[0].dev = flashDev;
+
+	/* store the mtd device for later use */
+	flashDev->genericDevice = mtd;
+
 	// Stuff to configure YAFFS
 	// Stuff to initialise anything special (eg lock semaphore).
 	yaffsfs_LocalInitialisation();
-	
+
 	// Set up devices
 
+/* XXX U-BOOT XXX */
 #if 0
 	// /ram
-	ramDev.nBytesPerChunk = YAFFS_BYTES_PER_CHUNK;
-	ramDev.nChunksPerBlock = YAFFS_CHUNKS_PER_BLOCK;
+	ramDev.nBytesPerChunk = 512;
+	ramDev.nChunksPerBlock = 32;
 	ramDev.nReservedBlocks = 2; // Set this smaller for RAM
-	ramDev.startBlock = 0; // Can now use block zero
-	ramDev.endBlock = 127; // Last block in 2MB.	
+	ramDev.startBlock = 1; // Can't use block 0
+	ramDev.endBlock = 127; // Last block in 2MB.
 	ramDev.useNANDECC = 1;
 	ramDev.nShortOpCaches = 0;	// Disable caching on this device.
 	ramDev.genericDevice = (void *) 0;	// Used to identify the device in fstat.
-	ramDev.writeChunkToNAND = yramdisk_WriteChunkToNAND;
-	ramDev.readChunkFromNAND = yramdisk_ReadChunkFromNAND;
+	ramDev.writeChunkWithTagsToNAND = yramdisk_WriteChunkWithTagsToNAND;
+	ramDev.readChunkWithTagsFromNAND = yramdisk_ReadChunkWithTagsFromNAND;
 	ramDev.eraseBlockInNAND = yramdisk_EraseBlockInNAND;
 	ramDev.initialiseNAND = yramdisk_InitialiseNAND;
 
 	// /boot
-	bootDev.nBytesPerChunk = YAFFS_BYTES_PER_CHUNK;
-	bootDev.nChunksPerBlock = YAFFS_CHUNKS_PER_BLOCK;
+	bootDev.nBytesPerChunk = 612;
+	bootDev.nChunksPerBlock = 32;
 	bootDev.nReservedBlocks = 5;
-	bootDev.startBlock = 0; // Can now use block zero
-	bootDev.endBlock = 127; // Last block in 2MB.	
+	bootDev.startBlock = 1; // Can't use block 0
+	bootDev.endBlock = 127; // Last block in 2MB.
 	bootDev.useNANDECC = 0; // use YAFFS's ECC
 	bootDev.nShortOpCaches = 10; // Use caches
 	bootDev.genericDevice = (void *) 1;	// Used to identify the device in fstat.
@@ -109,26 +161,57 @@ int yaffs_StartUp(void)
 	bootDev.initialiseNAND = yflash_InitialiseNAND;
 #endif
 
-	struct mtd_info *mtd = &nand_info[0];
 		// /flash
-	flashDev.nBytesPerChunk = YAFFS_BYTES_PER_CHUNK;
-	flashDev.nChunksPerBlock = YAFFS_CHUNKS_PER_BLOCK;
-	flashDev.nReservedBlocks = 5;
-	flashDev.startBlock = 10; // First block after 2MB
-	flashDev.endBlock = 40; // Last block in 16MB
-	flashDev.useNANDECC = 0; // use YAFFS's ECC
-	flashDev.nShortOpCaches = 10; // Use caches
-	flashDev.genericDevice = mtd;	// Used to identify the device in fstat.
-	flashDev.writeChunkToNAND = nandmtd_WriteChunkToNAND;
-	flashDev.readChunkFromNAND = nandmtd_ReadChunkFromNAND;
-	flashDev.eraseBlockInNAND = nandmtd_EraseBlockInNAND;
-	flashDev.initialiseNAND = nandmtd_InitialiseNAND;
+		
+	flashDev->nReservedBlocks = 5;
+//  flashDev->nShortOpCaches = (options.no_cache) ? 0 : 10;
+	flashDev->nShortOpCaches = 10; // Use caches
+	flashDev->useNANDECC = 0; // do not use YAFFS's ECC
+
+	if (yaffsVersion == 2)
+	{
+		flashDev->writeChunkWithTagsToNAND = nandmtd2_WriteChunkWithTagsToNAND;
+		flashDev->readChunkWithTagsFromNAND = nandmtd2_ReadChunkWithTagsFromNAND;
+		flashDev->markNANDBlockBad = nandmtd2_MarkNANDBlockBad;
+		flashDev->queryNANDBlock = nandmtd2_QueryNANDBlock;
+		flashDev->spareBuffer = YMALLOC(mtd->oobsize);
+		flashDev->isYaffs2 = 1;
+//#if (LINUX_VERSION_CODE > KERNEL_VERSION(2,6,17))
+#if 0
+		flashDev->nDataBytesPerChunk = mtd->writesize;
+		flashDev->nChunksPerBlock = mtd->erasesize / mtd->writesize;
+#else
+		//flashDev->nDataBytesPerChunk = mtd->oobblock;
+		flashDev->nDataBytesPerChunk = 2048;
+		flashDev->nChunksPerBlock = mtd->erasesize / mtd->oobblock;
+#endif
+		//nBlocks = mtd->size / mtd->erasesize;
+		nBlocks = 200 ;
+
+		flashDev->nCheckpointReservedBlocks = 10;
+		flashDev->startBlock = 50;
+		flashDev->endBlock = nBlocks - 1;
+	}
+	else
+	{
+		flashDev->writeChunkToNAND = nandmtd_WriteChunkToNAND;
+		flashDev->readChunkFromNAND = nandmtd_ReadChunkFromNAND;
+		flashDev->isYaffs2 = 0;
+		nBlocks = mtd->size / (YAFFS_CHUNKS_PER_BLOCK * YAFFS_BYTES_PER_CHUNK);
+		flashDev->startBlock = 320;
+		flashDev->endBlock = nBlocks - 1;
+		flashDev->nChunksPerBlock = YAFFS_CHUNKS_PER_BLOCK;
+		flashDev->nDataBytesPerChunk = YAFFS_BYTES_PER_CHUNK;
+	}
+
+	/* ... and common functions */
+	flashDev->eraseBlockInNAND = nandmtd_EraseBlockInNAND;
+	flashDev->initialiseNAND = nandmtd_InitialiseNAND;
 
 	yaffs_initialise(yaffsfs_config);
-	
+
 	return 0;
 }
-
 
 
 void make_a_file(char *yaffsName,char bval,int sizeOfFile)
@@ -242,7 +325,7 @@ void cmd_yaffs_mread_file(char *fn, char *addr)
 		return;
 	}
 
-	yaffs_read(h,addr,(int)s.yst_size);
+	yaffs_read(h,addr,(int)s.st_size);
 	printf("\t[DONE]\n");
 
 	yaffs_close(h);
@@ -289,7 +372,7 @@ void cmd_yaffs_ls(const char *mountpt, int longlist)
 			{
 				sprintf(tempstr, "%s/%s", mountpt, de->d_name);
 				yaffs_stat(tempstr, &stat);
-				printf("%-25s\t%7ld\n",de->d_name, stat.yst_size);
+				printf("%-25s\t%7ld\n",de->d_name, stat.st_size);
 			}
 			else
 			{
@@ -339,4 +422,3 @@ void cmd_yaffs_mv(const char *oldPath, const char *newPath)
 	if ( retval < 0)
 		printf("yaffs_unlink returning error: %d\n", retval);
 }
-
--- fs/yaffs2/yaffsinterface.h
+++ fs/yaffs2/yaffsinterface.h
@@ -1,8 +1,7 @@
 /*
- * YAFFS: Yet another FFS. A NAND-flash specific file system.
- * yaffsinterface.h: Interface to the guts of yaffs.
+ * YAFFS: Yet another Flash File System . A NAND-flash specific file system.
  *
- * Copyright (C) 2002 Aleph One Ltd.
+ * Copyright (C) 2002-2007 Aleph One Ltd.
  *   for Toby Churchill Ltd and Brightstar Engineering
  *
  * Created by Charles Manning <charles@aleph1.co.uk>
@@ -12,14 +11,11 @@
  * published by the Free Software Foundation.
  *
  * Note: Only YAFFS headers are LGPL, YAFFS C code is covered by GPL.
- *
  */
- 
+
 #ifndef __YAFFSINTERFACE_H__
 #define __YAFFSINTERFACE_H__
-
 
 int yaffs_Initialise(unsigned nBlocks);
 
 #endif
-
